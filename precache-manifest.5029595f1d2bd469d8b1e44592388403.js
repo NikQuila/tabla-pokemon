fd6b04105abc9fa0d18e113446ccf039a51e5396
NikQuila <nicopirozzi1@gmail.com>
2020-07-22 17:04:57 -0400

    Updates

---
+++ precache-manifest.5029595f1d2bd469d8b1e44592388403.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1822fa63644100b22f4f937d43e0fc6a",
+    "url": "/tabla-pokemon/index.html"
+  },
+  {
+    "revision": "bfef71cfd6c94e510dae",
+    "url": "/tabla-pokemon/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "88084ae5c0bd520532fb",
+    "url": "/tabla-pokemon/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bfef71cfd6c94e510dae",
+    "url": "/tabla-pokemon/static/js/2.61b604c6.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "/tabla-pokemon/static/js/2.61b604c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88084ae5c0bd520532fb",
+    "url": "/tabla-pokemon/static/js/main.54bece5f.chunk.js"
+  },
+  {
+    "revision": "308bc9cccbce75e64f86",
+    "url": "/tabla-pokemon/static/js/runtime-main.ceb54a7b.js"
+  }
+]);
